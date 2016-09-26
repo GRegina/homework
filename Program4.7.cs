2b1e5f8086ed9ac5b1746874d117c7379cf2a362
Regina <regi2748@inbox.ru>
2016-09-27 00:04:08 +0300

    no message

---
+++ Program4.7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Programs
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+
+		{
+			int k, n=0, i=0, t=0;
+			k = int.Parse(Console.ReadLine());
+			while (t==0)
+			{
+				i++;
+
+				for (int v = 1; v <= k; v++)
+				{
+					if (i % v == 0)
+						n = 1;
+					else
+					{
+						n = 0;
+						break;
+					}
+				}
+				if (n == 1)
+					t = 1;
+			}
+			Console.WriteLine("наименьший целый делитель = {0}", i);
+
+		} 
+	}
+}
